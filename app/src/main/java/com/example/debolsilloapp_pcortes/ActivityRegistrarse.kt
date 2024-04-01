1be0496ae0c06fa9d3758db8fcfb844ea5e01f27
ProgCortes2003 <programmercortes@gmail.com>
2024-04-01 10:00:17 -0500

    Ventana de login: faltan botones registrar e ingresar, añadir opcion de ingresar como invitado

---
+++ app/src/main/java/com/example/debolsilloapp_pcortes/ActivityRegistrarse.kt
@@ -0,0 +1,11 @@
+package com.example.debolsilloapp_pcortes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityRegistrarse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrarse)
+    }
+}
---
+++ app/src/main/java/com/example/debolsilloapp_pcortes/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.debolsilloapp_pcortes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.debolsilloapp_pcortes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
