1927b12919a9b4aa1806bffa93faace33e845944
ProgCortes2003 <programmercortes@gmail.com>
2024-05-28 11:24:22 -0500

    Terminar interfaz de Movimientos

--- app/src/main/java/com/example/appdebolsillousandojetpackcompose/appdebolsillo_movimientos.kt
+++ app/src/main/java/com/example/appdebolsillousandojetpackcompose/appdebolsillo_movimientos.kt
@@ -3,14 +3,25 @@ package com.example.appdebolsillousandojetpackcompose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuBoxScope
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -21,6 +32,8 @@ import androidx.navigation.NavController
 
 @Composable
 fun mostrarVentanaMovimientos(navController: NavController){
+
+
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -48,5 +61,7 @@ fun mostrarVentanaMovimientos(navController: NavController){
         Text(text = "Movimientos",
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold)
+        
+
     }
 }
--- app/src/main/java/com/example/appdebolsillousandojetpackcompose/ventanas_parametros/clasesVentanaParametros/Movimiento.kt
+++ app/src/main/java/com/example/appdebolsillousandojetpackcompose/ventanas_parametros/clasesVentanaParametros/Movimiento.kt
@@ -3,9 +3,24 @@ package com.example.appdebolsillousandojetpackcompose.ventanas_parametros.clases
 class Movimiento {
 
     var idMovimiento: String? = null
-    var nombreProveedor: String? = null
-    var nombreCliente: String? =  null
+    var fechaMovimiento: String = ""
+    var nombreProveedorOCliente =  ""
     var tipoDeMovimiento: String = ""
-    var productos: String = ""
+    var listadoProductos: String = ""
+    var totalMovimiento:Int = 0
+
+
+
+    constructor(
+                idMovimiento: String?,
+                fechaMovimiento:String,
+                nombreProveedorOCliente: String,
+                tipoDeMovimiento: String,
+                listadoProductos: String,
+                totalMovimiento:Int
+        ){
+
+    }
+
 
 }
